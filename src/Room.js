fe1080883b741b293dca4e32bdd9b34fa9e7de25
andy.vo <andyvo.techvify@gmail.com>
2023-01-30 15:59:12 +0700

    update room, remove share screen

--- src/Room.js
+++ src/Room.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import Participant from "./Participant";
-import Video from "twilio-video";
-
 
 const Room = ({ roomName, room, handleLogout }) => {
   const [participants, setParticipants] = useState([]);
-  const [screenParticipants, setScreenParticipants] = useState([])
 
   useEffect(() => {
     const participantConnected = (participant) => {
@@ -17,11 +14,6 @@ const Room = ({ roomName, room, handleLogout }) => {
         prevParticipants.filter((p) => p !== participant)
       );
     };
-    room.participants.forEach(participant => {
-      participant.on('trackAdded', track => {
-        console.log(track)
-      });
-    });
 
     room.on("participantConnected", participantConnected);
     room.on("participantDisconnected", participantDisconnected);
@@ -35,36 +27,10 @@ const Room = ({ roomName, room, handleLogout }) => {
   const remoteParticipants = participants.map((participant) => (
     <Participant key={participant.sid} participant={participant} />
   ));
-  // var screenTrack
-  const [screenTrack, setScreenTrack] = useState()
 
-  const  shareScreenHandler = useCallback(() => {
-    if (!screenTrack) {
-      navigator.mediaDevices.getDisplayMedia().then(stream => {
-          setScreenTrack(new Video.LocalVideoTrack(stream.getTracks()[0],{name: "screen"}));
-          console.log(room)
-
-      }).catch(() => {
-          alert('Could not share the screen.');
-      });
-  }
-  else {
-      room.localParticipant.unpublishTrack(screenTrack );
-      screenTrack.stop();
-      setScreenTrack(null);
-  }
-
-});
-useEffect(() => {
-  if(screenTrack) {
-    room.localParticipant.publishTrack(screenTrack);
-    screenTrack.mediaStreamTrack.onended = () => { shareScreenHandler() };
-  }
-}, [screenTrack, shareScreenHandler,room.localParticipant])
   return (
     <div className="room">
       <h2>Room: {roomName}</h2>
-      <button onClick={shareScreenHandler}> Share screen</button>
       <button onClick={handleLogout}>Log out</button>
       <div className="local-participant">
         {room ? (
@@ -75,7 +41,6 @@ useEffect(() => {
         ) : (
           ""
         )}
-        {room.localParticipant && <Participant key="test" participant={screenParticipants}></Participant>}
       </div>
       <h3>Remote Participants</h3>
       <div className="remote-participants">{remoteParticipants}</div>
